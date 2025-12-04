50068921a22042c2d2b40440d7c1437ddd7c2b0b
Vikas Yadav <vikasyadav@Vikass-MacBook-Pro.local>
2025-12-04 23:49:27 +0530

    updated day 6 part 1

---
+++ day6/part1.js
@@ -0,0 +1,45 @@
+// const map = `COM)B
+// B)C
+// C)D
+// D)E
+// E)F
+// B)G
+// G)H
+// D)I
+// E)J
+// J)K
+// K)L`;
+
+const map = Deno.readTextFileSync("./input.txt");
+
+const countSteps = (countsObject, key) => {
+  if (countsObject[key] === undefined) return 0;
+  if (countsObject[key] === parseInt(countsObject[key])) {
+    return countsObject[key];
+  }
+  return countSteps(countsObject, countsObject[key]) + 1;
+};
+
+const parseMapData = (map) => {
+  return map.split("\n").map((x) => x.split(")"));
+};
+
+const dataInObject = (planetsWithOrbits) => {
+  const countsObject = {};
+  planetsWithOrbits.forEach((element) => {
+    countsObject[element[1]] = element[0];
+  });
+  return countsObject;
+};
+
+const noOfOrbits = (map) => {
+  const planetsWithOrbits = parseMapData(map);
+  const countsObject = dataInObject(planetsWithOrbits);
+  for (let index = 0; index < planetsWithOrbits.length; index++) {
+    const key = planetsWithOrbits[index][1];
+    countsObject[key] = countSteps(countsObject, key);
+  }
+  return Object.values(countsObject).reduce((a, b) => a + b);
+};
+
+console.log(noOfOrbits(map));
